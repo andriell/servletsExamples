0cacf0c7035cfaecf23aab27d38d0d3338ddaf2d
andriell <root.andriell@gmail.com>
2015-11-11 14:12:00 +0300

    message

--- src/ru/loryPress/ConfigDemoServlet.java
+++ src/ru/loryPress/ConfigDemoServlet.java
@@ -2,14 +2,18 @@ package ru.loryPress;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Enumeration;
 
 /**
  * Created by andriell on 11.11.15.
  */
 public class ConfigDemoServlet implements Servlet {
+    ServletConfig servletConfig;
+
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
+        this.servletConfig = servletConfig;
         Enumeration parameters = servletConfig.getInitParameterNames();
         while (parameters.hasMoreElements()) {
             String parameterName = (String) parameters.nextElement();
@@ -20,12 +24,18 @@ public class ConfigDemoServlet implements Servlet {
 
     @Override
     public ServletConfig getServletConfig() {
-        return null;
+        return servletConfig;
     }
 
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-
+        PrintWriter out = servletResponse.getWriter();
+        Enumeration parameters = getServletConfig().getInitParameterNames();
+        while (parameters.hasMoreElements()) {
+            String parameterName = (String) parameters.nextElement();
+            out.println("Parameter name: " + parameterName);
+            out.println("Parameter value: " + servletConfig.getInitParameter(parameterName));
+        }
     }
 
     @Override
