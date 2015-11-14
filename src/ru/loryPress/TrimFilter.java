8d15b64da7dde2333c2add70c4b3a9541be7a1ec
andriell <root.andriell@gmail.com>
2015-11-14 23:33:51 +0300

    message

---
+++ src/ru/loryPress/TrimFilter.java
@@ -0,0 +1,47 @@
+package ru.loryPress;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Enumeration;
+
+/**
+ * Created by andriell on 14.11.15.
+ */
+public class TrimFilter implements Filter {
+    private FilterConfig filterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("TrimFilter.init");
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("TrimFilter.doFilter");
+        if (! (servletRequest instanceof HttpServletRequest && servletResponse instanceof HttpServletResponse)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+
+        Enumeration<String> getParam = req.getParameterNames();
+        while(getParam.hasMoreElements()) {
+            String paramName = getParam.nextElement();
+            String[] paramValues = req.getParameterValues(paramName);
+            for (int i = 0; i < paramValues.length; i++) {
+                paramValues[i] = paramValues[i].trim();
+            }
+            req.setAttribute(paramName + "_all", paramValues);
+            req.setAttribute(paramName + "_first", paramValues[0]);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("TrimFilter.destroy");
+    }
+}
