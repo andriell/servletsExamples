e88d06045ca754f2632dfdd630153aea74fc9ee6
andriell <root.andriell@gmail.com>
2015-11-14 23:50:43 +0300

    message

---
+++ src/ru/loryPress/LogListener.java
@@ -0,0 +1,66 @@
+package ru.loryPress;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Created by andriell on 14.11.15.
+ */
+public class LogListener implements ServletContextListener,
+        ServletContextAttributeListener,
+        HttpSessionListener, HttpSessionAttributeListener {
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("ServletContextListener.contextInitialized");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("ServletContextListener.contextDestroyed");
+    }
+
+
+    @Override
+    public void attributeAdded(ServletContextAttributeEvent servletContextAttributeEvent) {
+        System.out.println("ServletContextAttributeListener.attributeAdded");
+    }
+
+    @Override
+    public void attributeRemoved(ServletContextAttributeEvent servletContextAttributeEvent) {
+        System.out.println("ServletContextAttributeListener.attributeRemoved");
+    }
+
+    @Override
+    public void attributeReplaced(ServletContextAttributeEvent servletContextAttributeEvent) {
+        System.out.println("ServletContextAttributeListener.attributeReplaced");
+    }
+
+
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+        System.out.println("HttpSessionListener.sessionCreated");
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        System.out.println("HttpSessionListener.sessionDestroyed");
+    }
+
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent httpSessionBindingEvent) {
+        System.out.println("HttpSessionAttributeListener.attributeAdded");
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent httpSessionBindingEvent) {
+        System.out.println("HttpSessionAttributeListener.attributeRemoved");
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent httpSessionBindingEvent) {
+        System.out.println("HttpSessionAttributeListener.attributeReplaced");
+    }
+}
